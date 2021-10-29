300cb3b2c237989440f15cc4352ddbe7522ba5d4
Davide Rossi (4I) <DAVIDE.ROSSI.4i@local.ittcesena.it>
2021-10-29 09:41:58 +0200

    Classe CD

---
+++ progettoCD/progettoCD/CD.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progettoCD
+{
+    class CD
+    {
+        private string _titolo;
+
+        public string Titolo
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Titolo non valido");
+                }
+
+                _titolo = value;
+            }
+
+            get
+            {
+                return _titolo;
+            }
+        }
+
+        private string _autore;
+        public string Autore
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Autore non valido");
+                }
+                _autore = value;
+            }
+
+            get
+            {
+                return _autore;
+            }
+        }
+
+
+        private string _etichetta;
+        public string Etichetta
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Etichetta non valida");
+                }
+
+                _etichetta = value;
+            }
+
+            get
+            {
+                return _etichetta;
+            }
+        }
+
+
+        public List<Brano> Brani { set; get; }
+
+        public CD(string titolo, string autore, string etichetta, List<Brano> brani)
+        {
+            Titolo = titolo;
+            Autore = autore;
+            Etichetta = etichetta;
+            Etichetta = etichetta;
+            Brani = brani;
+        }
+
+
+        public override string ToString()
+        {
+            //tutti i titoli dei brani
+            string ritorno = "";
+
+            foreach (Brano b in Brani)
+            {
+                ritorno += b.Titolo + " ";
+            }
+
+            return ritorno;
+        }
+
+        public double Durata()
+        {
+            double durata = 0;
+
+            foreach (Brano b in Brani)
+            {
+                durata += b.Durata;
+            }
+
+            return durata;
+        }
+    }
+}
