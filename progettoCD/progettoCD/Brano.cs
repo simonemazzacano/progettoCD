8564c05021e3dee18b084e5a1e55a75d5e97beed
Simone Mazzacano <SIMONE.MAZZACANO@local.ittcesena.it>
2021-10-29 09:13:01 +0200

    Classe brano completa

--- progettoCD/progettoCD/Brano.cs
+++ progettoCD/progettoCD/Brano.cs
@@ -18,10 +18,33 @@ namespace progettoCD
             Durata = durata;
         }
 
-        public string Titolo { get => _titolo; set => _titolo = value; }
-        public string Autore { get => _autore; set => _autore = value; }
-        public int Durata { get => _durata; set => _durata = value; }
-
+        public string Titolo 
+        { 
+            get => _titolo; 
+            private set //il set non serve a parte nel costruttore
+            {
+                if (string.IsNullOrEmpty(value)) throw new Exception("Titolo non valido");
+                _titolo = value;
+            }
+        }
+        public string Autore
+        {
+            get => _autore;
+            private set//il set non serve a parte nel costruttore
+            {
+                if (string.IsNullOrEmpty(value)) throw new Exception("Autore non valido");
+                _autore = value;
+            }
+        }
+        public int Durata 
+        { 
+            get => _durata;
+            private set
+            {
+                if (value < 0) throw new Exception("Durata brano negativa");
+                _durata = value;
+            }
+        }
 
         public bool ShortSong(int durata)
         {
@@ -29,7 +52,8 @@ namespace progettoCD
         }
         public override string ToString()
         {
-            return "";
+            return $"(Brano)\n" +
+                $"Titolo: {Titolo}\nAutore: {Autore}\nDurata: {Durata} sec";
         }
     }
 }
