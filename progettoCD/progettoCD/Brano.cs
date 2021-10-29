7003cdb5747b0d536b8a37db4b4becd7d91ab3cd
Simone Mazzacano <SIMONE.MAZZACANO@local.ittcesena.it>
2021-10-29 08:57:16 +0200

    Template classe brano

--- progettoCD/progettoCD/Brano.cs
+++ progettoCD/progettoCD/Brano.cs
@@ -8,5 +8,28 @@ namespace progettoCD
 {
     class Brano
     {
+        private string _titolo, _autore;
+        private int _durata;
+
+        public Brano(string titolo, string autore, int durata)
+        {
+            Titolo = titolo;
+            Autore = autore;
+            Durata = durata;
+        }
+
+        public string Titolo { get => _titolo; set => _titolo = value; }
+        public string Autore { get => _autore; set => _autore = value; }
+        public int Durata { get => _durata; set => _durata = value; }
+
+
+        public bool ShortSong(int durata)
+        {
+            return Durata < durata;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
